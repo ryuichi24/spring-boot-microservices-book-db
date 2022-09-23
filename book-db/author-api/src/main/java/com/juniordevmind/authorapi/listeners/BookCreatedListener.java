918fb64931060731fc419d578fd7a7b93b43f2b4
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-09-23 20:53:15 +0900

    add Book table in author api

--- book-db/author-api/src/main/java/com/juniordevmind/authorapi/listeners/BookCreatedListener.java
+++ book-db/author-api/src/main/java/com/juniordevmind/authorapi/listeners/BookCreatedListener.java
@@ -1,8 +1,12 @@
 package com.juniordevmind.authorapi.listeners;
 
+import java.util.Optional;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.juniordevmind.authorapi.models.Book;
+import com.juniordevmind.authorapi.repositories.BookRepository;
 import com.juniordevmind.shared.constants.RabbitMQKeys;
 import com.juniordevmind.shared.domain.BookEventDto;
 import com.juniordevmind.shared.models.CustomMessage;
@@ -14,8 +18,20 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class BookCreatedListener {
+    private final BookRepository _bookRepository;
+
     @RabbitListener(queues = RabbitMQKeys.AUTHOR_API_BOOK_CREATED_QUEUE)
     public void handleMessage(CustomMessage<BookEventDto> message) {
         log.info("{} got triggered. Message: {}", BookCreatedListener.class, message.toString());
+        BookEventDto bookEventDto = message.getPayload();
+        Optional<Book> result = _bookRepository.findById(bookEventDto.getId());
+        if (result.isPresent()) {
+            return;
+        }
+        Book newBook = new Book();
+        newBook.setId(bookEventDto.getId());
+        newBook.setTitle(bookEventDto.getTitle());
+        newBook.setDescription(bookEventDto.getDescription());
+        _bookRepository.save(newBook);
     }
 }
--- book-db/shared/src/main/java/com/juniordevmind/shared/constants/RabbitMQKeys.java
+++ book-db/shared/src/main/java/com/juniordevmind/shared/constants/RabbitMQKeys.java
@@ -6,7 +6,7 @@ public class RabbitMQKeys {
     public static final String BOOK_API_AUTHOR_CREATED_QUEUE = "book-api.message.queue.author.created";
     public static final String COMMENT_API_AUTHOR_CREATED_QUEUE = "comment-api.message.queue.author.created";
     // book created event
-    public static final String BOOK_CREATED_EXCHANGE = "message.exchange.fanout.boo.created";
+    public static final String BOOK_CREATED_EXCHANGE = "message.exchange.fanout.book.created";
     public static final String AUTHOR_API_BOOK_CREATED_QUEUE = "author-api.message.queue.book.created";
     public static final String COMMENT_API_BOOK_CREATED_QUEUE = "comment-api.message.queue.book.created";
 }
