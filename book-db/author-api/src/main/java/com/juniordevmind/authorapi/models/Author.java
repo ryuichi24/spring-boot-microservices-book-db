63171736377ecb296874e90ba0d36ac7f55a5089
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-09-24 11:20:03 +0900

    add author_books table

--- book-db/author-api/src/main/java/com/juniordevmind/authorapi/models/Author.java
+++ book-db/author-api/src/main/java/com/juniordevmind/authorapi/models/Author.java
@@ -1,7 +1,12 @@
 package com.juniordevmind.authorapi.models;
 
+import java.util.List;
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 import com.juniordevmind.shared.models.EntityBase;
@@ -24,4 +29,9 @@ public class Author extends EntityBase {
     private String name;
     @Column(name = "description", nullable = false)
     private String description;
+    // https://stackoverflow.com/a/57241586/13723015
+    @ElementCollection
+    @CollectionTable(name = "author_books", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "book_id ")
+    private List<String> books;
 }
