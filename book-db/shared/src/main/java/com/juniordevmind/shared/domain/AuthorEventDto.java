823b8cdf29fb4f8c0ee62124318463d94afb92e0
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-09-24 15:32:52 +0900

    minor change

--- book-db/shared/src/main/java/com/juniordevmind/shared/domain/AuthorEventDto.java
+++ book-db/shared/src/main/java/com/juniordevmind/shared/domain/AuthorEventDto.java
@@ -1,13 +1,13 @@
 package com.juniordevmind.shared.domain;
 
-import com.juniordevmind.shared.models.EventDtoBase;
+import com.juniordevmind.shared.models.DtoBase;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class AuthorEventDto extends EventDtoBase {
+public class AuthorEventDto extends DtoBase {
     private String name;
     private String description;
 }
--- book-db/shared/src/main/java/com/juniordevmind/shared/domain/BookEventDto.java
+++ book-db/shared/src/main/java/com/juniordevmind/shared/domain/BookEventDto.java
@@ -1,13 +1,13 @@
 package com.juniordevmind.shared.domain;
 
-import com.juniordevmind.shared.models.EventDtoBase;
+import com.juniordevmind.shared.models.DtoBase;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class BookEventDto extends EventDtoBase {
+public class BookEventDto extends DtoBase {
     private String title;
     private String description;
 }
