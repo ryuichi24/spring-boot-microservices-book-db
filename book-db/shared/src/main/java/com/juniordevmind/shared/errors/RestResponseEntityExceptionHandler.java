e5b2ca855004ed75d87df7e98b5fcbd3ff4016e3
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-09-24 15:03:34 +0900

    update error handler

--- book-db/shared/src/main/java/com/juniordevmind/shared/errors/RestResponseEntityExceptionHandler.java
+++ book-db/shared/src/main/java/com/juniordevmind/shared/errors/RestResponseEntityExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -28,14 +29,26 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
-        BindingResult bindingResult = ex.getBindingResult();
+        ErrorResponse errorResponse = _buildErrorResponse(ex.getBindingResult());
+        return this.handleExceptionInternal(ex, errorResponse, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return this.handleExceptionInternal(ex,
+                new ErrorResponse(new ArrayList<>(Arrays.asList(ex.getMessage())), HttpStatus.BAD_REQUEST), headers,
+                HttpStatus.BAD_REQUEST, request);
+    }
+
+    private ErrorResponse _buildErrorResponse(BindingResult bindingResult) {
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         List<String> errorMessages = new ArrayList<>();
-        for(final FieldError error: fieldErrors) {
+        for (final FieldError error : fieldErrors) {
             errorMessages.add(error.getField() + ": " + error.getDefaultMessage());
         }
         ErrorResponse errorResponse = new ErrorResponse(errorMessages, HttpStatus.BAD_REQUEST);
-        return this.handleExceptionInternal(ex, errorResponse, headers, HttpStatus.BAD_REQUEST, request);
+        return errorResponse;
     }
 
 }
