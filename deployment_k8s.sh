0da5ec0c7df2dda0430fb3cdc7c807642b471dfe
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-10-01 22:55:14 +0900

    update k8s manifest for dynamic image

--- deployment_k8s.sh
+++ deployment_k8s.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+CONTAINER_REGISTRY="docker.io"
+ACCOUNT_NAME="ryuichi24"
+IMAGE_TAG="cd81e6"
+
+AUTHOR_SERVICE_CONTAINER_IMAGE="${CONTAINER_REGISTRY}/${ACCOUNT_NAME}/bookdb-author-service:${IMAGE_TAG}"
+BOOK_SERVICE_CONTAINER_IMAGE="${CONTAINER_REGISTRY}/${ACCOUNT_NAME}/bookdb-book-service:${IMAGE_TAG}"
+COMMENT_SERVICE_CONTAINER_IMAGE="${CONTAINER_REGISTRY}/${ACCOUNT_NAME}/bookdb-comment-service:${IMAGE_TAG}"
+
 # databases
 kubectl apply -f k8s/author-api-mysql-depl.yml
 kubectl apply -f k8s/book-api-mysql-depl.yml
@@ -8,10 +16,13 @@ kubectl apply -f k8s/comment-api-mysql-depl.yml
 # message broker
 kubectl apply -f k8s/rabbitmq-depl.yml
 
+# https://stackoverflow.com/questions/68888449/how-to-define-image-name-in-kubernetes-manifest-deployment-yml-file-dynamically
+# https://stackoverflow.com/questions/54032336/need-some-explaination-of-kubectl-stdin-and-pipe
 # services
-kubectl apply -f k8s/author-api-depl.yml
-kubectl apply -f k8s/book-api-depl.yml
-kubectl apply -f k8s/comment-api-depl.yml
+sed "s,__DOCKER_IMAGE__,${AUTHOR_SERVICE_CONTAINER_IMAGE}," k8s/author-api-depl.yml | kubectl apply -f -
+sed "s,__DOCKER_IMAGE__,${BOOK_SERVICE_CONTAINER_IMAGE}," k8s/book-api-depl.yml | kubectl apply -f -
+sed "s,__DOCKER_IMAGE__,${COMMENT_SERVICE_CONTAINER_IMAGE}," k8s/comment-api-depl.yml | kubectl apply -f -
 
 # api gateway
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f k8s/ingress-srv.yml
